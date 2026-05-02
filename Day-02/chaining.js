a205f80c3190586700bd07cbd042b6e342cf8db8
Tusar78 <tusarahmed141@gmail.com>
2026-05-03 02:05:21 +0600

    Solve one chaining problem

---
+++ Day-02/chaining.js
@@ -0,0 +1,19 @@
+const products = [
+  { name: "Cake", price: 500, category: "dessert", inStock: true },
+  { name: "Burger", price: 200, category: "fast-food", inStock: false },
+  { name: "Pizza", price: 700, category: "fast-food", inStock: true },
+  { name: "Ice Cream", price: 150, category: "dessert", inStock: true },
+];
+
+const finalProducts = products
+  .filter((product) => product.inStock)
+  .filter((product) => product.price > 200)
+  .map((product) => {
+    return {
+        ...product,
+        price: Number((product.price * 1.1).toFixed(2))
+    };
+  });
+
+  console.log(finalProducts);
+  
